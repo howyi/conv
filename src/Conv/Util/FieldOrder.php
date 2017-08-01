76a2cde5881c5856bd37f02f287cf97fdb3b800a
howyi <qrbys@outlook.com>
2017-08-02 00:44:34 +0900

    add Util tests

--- src/Conv/Util/FieldOrder.php
+++ src/Conv/Util/FieldOrder.php
@@ -46,5 +46,4 @@ class FieldOrder
     {
         return $this->nextAfterField;
     }
-
 }
---
+++ tests/Conv/Util/OperatorTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Conv\Util;
+
+use Symfony\Component\Console\Helper\QuestionHelper;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+
+class OperatorTest extends \PHPUnit\Framework\TestCase
+{
+    private $prophet;
+
+    protected function setup()
+    {
+        $this->prophet = new \Prophecy\Prophet;
+    }
+
+    protected function tearDown()
+    {
+        $this->prophet->checkPredictions();
+    }
+
+    public function testConstruct()
+    {
+        $helper = $this->prophet->prophesize(QuestionHelper::class);
+        $input = $this->prophet->prophesize(InputInterface::class);
+        $output = $this->prophet->prophesize(OutputInterface::class);
+        $operator = new Operator($helper->reveal(), $input->reveal(), $output->reveal());
+
+        $answer = 'answer';
+        $helper->ask($input, $output, \Prophecy\Argument::type(ChoiceQuestion::class))
+            ->willReturn($answer)
+            ->shouldBeCalledTimes(1);
+        $this->assertSame($answer, $operator->choiceQuestion('question', ['answer']));
+
+        $message = 'message';
+        $output->writeln($message)->shouldBeCalledTimes(1);
+        $operator->output($message);
+    }
+}
