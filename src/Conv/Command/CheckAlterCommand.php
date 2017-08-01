b0cb2108fd793ad7b80673919183f1e400793ac6
howyi <qrbys@outlook.com>
2017-08-01 17:54:37 +0900

    fix check alter command

---
+++ src/Conv/Command/CheckAlterCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Conv\Command;
+
+use Conv\Factory\DatabaseStructureFactory;
+use Conv\Factory\TableStructureFactory;
+use Conv\Generator\MigrationGenerator;
+use Conv\Generator\TableAlterMigrationGenerator;
+use Conv\Util\Operator;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CheckAlterCommand extends AbstractCommand
+{
+    protected function configure()
+    {
+        $this->setName('check:alter');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $operator = new Operator(
+            $this->getHelper('question'),
+            $input,
+            $output
+        );
+
+        $actualStructure = DatabaseStructureFactory::fromPDO($this->getPDO('conv'));
+        $expectStructure = DatabaseStructureFactory::fromDir('schema');
+        $alter = MigrationGenerator::generate(
+            $actualStructure,
+            $expectStructure,
+            $operator
+        );
+
+        $pdo = $this->getPDO('conv');
+        foreach ($alter->getMigrationList() as $migration) {
+            $operator->output('<fg=green>実行クエリ</>');
+            $operator->output($migration->getUp());
+            $pdo->exec($migration->getUp());
+        }
+
+        $actualStructure = DatabaseStructureFactory::fromPDO($pdo);
+        $alter = MigrationGenerator::generate(
+            $actualStructure,
+            $expectStructure,
+            $operator
+        );
+
+        $operator->output(sprintf('<fg=cyan>DBとの差分:%d</>', count($alter->getMigrationList())));
+    }
+}
