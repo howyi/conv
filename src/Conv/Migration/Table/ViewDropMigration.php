efda5821aacd480b904695ccb0a8074f1b11e02f
howyi <qrbys@outlook.com>
2017-08-13 23:13:47 +0900

    add view create-drop migration

---
+++ src/Conv/Migration/Table/ViewDropMigration.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Conv\Migration\Table;
+
+use Conv\Structure\ViewStructure;
+
+/**
+ * DROP VIEW
+ */
+class ViewDropMigration extends AbstractTableMigration
+{
+    /**
+     * @param ViewStructure $viewStructure
+     */
+    public function __construct(
+        ViewStructure $viewStructure
+    ) {
+        $this->tableName = $viewStructure->getViewName();
+        $this->type = MigrationType::DROP;
+
+        $viewCreateMigration = new ViewCreateMigration($viewStructure);
+        $this->up = $viewCreateMigration->getDown();
+        $this->down = $viewCreateMigration->getUp();
+    }
+}
