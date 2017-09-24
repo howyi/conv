6acc2be76ee73f1324fc135e4d4cc83973cd470b
howyi <qrbys@outlook.com>
2017-09-24 23:39:59 +0900

    add view rename migration

---
+++ src/Conv/Migration/Table/ViewRenameMigration.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Conv\Migration\Table;
+
+use Conv\MigrationType;
+use Conv\Structure\ViewStructureInterface;
+
+/**
+ * RENAME TABLE ~ TO ~
+ */
+class ViewRenameMigration extends AbstractTableMigration
+{
+    private $isAltered = false;
+
+    /**
+     * @param ViewStructureInterface $beforeView
+     * @param ViewStructureInterface $afterView
+     * @param MigrationLineList $migrationLineList
+     */
+    public function __construct(
+        ViewStructureInterface $beforeView,
+        ViewStructureInterface $afterView
+    ) {
+        $this->tableName = $afterView->getViewName();
+        $this->type = MigrationType::VIEW_RENAME;
+
+        $beforeName = $beforeView->getViewName();
+        $afterName = $afterView->getViewName();
+
+        $this->up = "RENAME TABLE `$beforeName` TO `$afterName`;";
+        $this->down = "RENAME TABLE `$afterName` TO `$beforeName`;";
+    }
+}
