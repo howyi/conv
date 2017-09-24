ab52b614304a2def32cb05294398c93f88ed70fa
howyi <qrbys@outlook.com>
2017-09-24 23:43:01 +0900

    add view migration generator

--- src/Conv/MigrationType.php
+++ src/Conv/MigrationType.php
@@ -4,7 +4,9 @@ namespace Conv;
 
 class MigrationType
 {
-    const CREATE = 0;
-    const ALTER  = 1;
-    const DROP   = 2;
+    const CREATE            = 0;
+    const ALTER             = 1;
+    const DROP              = 2;
+    const CREATE_OR_REPLACE = 3;
+    const VIEW_RENAME       = 4;
 }
