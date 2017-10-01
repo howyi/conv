7fc42913d3e9d09e44706dc66d80a6fc02a3f3b4
howyi <qrbys@outlook.com>
2017-10-02 00:03:15 +0900

    add Partition types

---
+++ src/Conv/Util/PartitionType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Conv;
+
+class PartitionType
+{
+    const SHORT = 0;
+    const LONG  = 1;
+
+    const KEY           = 'key';
+    const HASH          = 'hash';
+    const LIST          = 'list';
+    const LIST_COLUMNS  = 'list_columns';
+    const RANGE         = 'range';
+    const RANGE_COLUMNS = 'range_columns';
+
+    const METHOD = [
+      'KEY'           => '',
+      'HASH'          => '',
+      'LIST'          => '',
+      'LIST COLUMNS'  => '',
+      'RANGE COLUMNS' => '',
+      'RANGE COLUMNS' => '',
+    ];
+
+    const METHOD_TYPE = [
+      'KEY'           => self::SHORT,
+      'HASH'          => self::SHORT,
+      'LIST'          => self::LONG,
+      'LIST COLUMNS'  => self::LONG,
+      'RANGE COLUMNS' => self::LONG,
+      'RANGE COLUMNS' => self::LONG,
+    ];
+}
