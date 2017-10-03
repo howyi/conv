99eb0d4c23c5fc482d82e5fafce6dc1fc6b6d034
howyi <qrbys@outlook.com>
2017-10-03 19:18:27 +0900

    add Linear hash type

--- src/Conv/Util/PartitionType.php
+++ src/Conv/Util/PartitionType.php
@@ -8,24 +8,30 @@ class PartitionType
     const LONG  = 1;
 
     const KEY           = 'key';
+    const LINEAR_KEY    = 'linear_key';
     const HASH          = 'hash';
+    const LINEAR_HASH   = 'linear_hash';
     const LIST          = 'list';
     const LIST_COLUMNS  = 'list_columns';
     const RANGE         = 'range';
     const RANGE_COLUMNS = 'range_columns';
 
     const METHOD = [
-      'KEY'           => '',
-      'HASH'          => '',
-      'LIST'          => '',
-      'LIST COLUMNS'  => '',
-      'RANGE'         => '',
-      'RANGE COLUMNS' => '',
+      'KEY'           => self::KEY,
+      'LINEAR KEY'    => self::LINEAR_KEY,
+      'HASH'          => self::HASH,
+      'LINEAR HASH'   => self::LINEAR_HASH,
+      'LIST'          => self::LIST,
+      'LIST COLUMNS'  => self::LIST_COLUMNS,
+      'RANGE'         => self::RANGE,
+      'RANGE COLUMNS' => self::RANGE_COLUMNS,
     ];
 
     const METHOD_TYPE = [
       'KEY'           => self::SHORT,
+      'LINEAR KEY'    => self::SHORT,
       'HASH'          => self::SHORT,
+      'LINEAR HASH'   => self::SHORT,
       'LIST'          => self::LONG,
       'LIST COLUMNS'  => self::LONG,
       'RANGE'         => self::LONG,
