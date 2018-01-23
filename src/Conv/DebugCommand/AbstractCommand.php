cd31c015958f17b0954412899a887f4d2d62436f
howyi <qrbys@outlook.com>
2018-01-23 21:19:28 +0900

    Change localhost IP

--- src/Conv/DebugCommand/AbstractCommand.php
+++ src/Conv/DebugCommand/AbstractCommand.php
@@ -12,9 +12,9 @@ abstract class AbstractCommand extends Command
     public function getPDO($dbname = null): \PDO
     {
         if (is_null($dbname)) {
-            $pdo = new \PDO('mysql:host=localhost;charset=utf8;', 'root', '');
+            $pdo = new \PDO('mysql:host=127.0.0.1;charset=utf8;', 'root', '');
         } else {
-            $pdo = new \PDO("mysql:host=localhost;dbname=$dbname;charset=utf8;", 'root', '');
+            $pdo = new \PDO("mysql:host=127.0.0.1;dbname=$dbname;charset=utf8;", 'root', '');
         }
         $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         return $pdo;
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,6 +3,6 @@ set_time_limit(0);
 
 require dirname(__FILE__).'/../vendor/autoload.php';
 
-$pdo = new \PDO('mysql:host=localhost;charset=utf8;', 'root', '');
+$pdo = new \PDO('mysql:host=127.0.0.1;charset=utf8;', 'root', '');
 $pdo->exec('DROP DATABASE IF EXISTS conv_test');
 $pdo->exec('CREATE DATABASE conv_test');
