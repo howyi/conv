dae009f624b65d610e1211983d6ee3505bf90715
howyi <qrbys@outlook.com>
2018-11-05 16:34:40 +0900

    fix namespaces

--- src/DebugCommand/CheckAlterCommand.php
+++ src/DebugCommand/CheckAlterCommand.php
@@ -3,11 +3,8 @@
 namespace Laminaria\Conv\DebugCommand;
 
 use Laminaria\Conv\DatabaseStructureFactory;
-use Laminaria\Conv\Factory\TableStructureFactory;
 use Laminaria\Conv\MigrationGenerator;
-use Laminaria\Conv\Generator\TableAlterMigrationGenerator;
 use Laminaria\Conv\Operator\DropOnlySilentOperator;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
