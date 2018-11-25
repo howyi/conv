69d93dbf4e2948c4d9aa588b201aae4d1a802d13
howyi <qrbys@outlook.com>
2018-11-25 23:12:48 +0900

    selectable port

--- tests/MigrationGeneratorSingleTest.php
+++ tests/MigrationGeneratorSingleTest.php
@@ -8,17 +8,11 @@ use Laminaria\Conv\Structure\DatabaseStructure;
 
 class MigrationGeneratorSingleTest extends \PHPUnit\Framework\TestCase
 {
-    private $pdo;
     private $prophet;
-    private $mysqlVersion;
 
     protected function setup()
     {
-        $this->pdo = TestUtility::getPdo('conv_test');
         $this->prophet = new \Prophecy\Prophet();
-        $this->mysqlVersion = $this->pdo->getAttribute(\PDO::ATTR_SERVER_VERSION);
-        preg_match("/^[0-9\.]+/", $this->mysqlVersion, $match);
-        $this->mysqlVersion = $match[0];
     }
 
     protected function tearDown()
@@ -28,78 +22,84 @@ class MigrationGeneratorSingleTest extends \PHPUnit\Framework\TestCase
 
     /**
      * @dataProvider generateProvider
-     * @param $name
-     * @param $beforeDir
-     * @param $afterDir
-     * @param $upPath
-     * @param $downPath
+     * @param string $name
+     * @param string $beforeDir
+     * @param string $afterDir
+     * @param string $upPath
+     * @param string $downPath
      */
     public function testGenerate($name, $beforeDir, $afterDir, $upPath, $downPath)
     {
-        $exploded = explode(':', $name);
-        $target = $exploded[1] ?? null;
+        foreach (TestUtility::getPdoArray() as $pdo) {
+            $mysqlVersion = $pdo->getAttribute(\PDO::ATTR_SERVER_VERSION);
+            preg_match("/^[0-9\.]+/", $mysqlVersion, $match);
+            $mysqlVersion = $match[0];
 
-        if (!is_null($target)) {
-            if (!Semver::satisfies($this->mysqlVersion, $target)) {
-                // Skipped
-                $this->assertTrue(true);
-                return;
+            $exploded = explode(':', $name);
+            $target = $exploded[1] ?? null;
+
+            if (!is_null($target)) {
+                if (!Semver::satisfies($mysqlVersion, $target)) {
+                    // Skipped
+                    $this->assertTrue(true);
+                    return;
+                }
             }
-        }
-        if (file_exists($afterDir)) {
-            $after = DatabaseStructureFactory::fromSqlDir(
-                $this->pdo,
-                $afterDir,
+            if (file_exists($afterDir)) {
+                $after = DatabaseStructureFactory::fromSqlDir(
+                    $pdo,
+                    $afterDir,
+                    new DropOnlySilentOperator()
+                );
+            } else {
+                $after = new DatabaseStructure([]);
+            }
+
+            if (file_exists($beforeDir)) {
+                $before = DatabaseStructureFactory::fromSqlDir(
+                    $pdo,
+                    $beforeDir,
+                    new DropOnlySilentOperator(),
+                    null,
+                    false
+                );
+            } else {
+                $pdo->exec('DROP DATABASE IF EXISTS ' . DatabaseStructureFactory::TMP_DBNAME);
+                $pdo->exec('CREATE DATABASE ' . DatabaseStructureFactory::TMP_DBNAME);
+                $before = new DatabaseStructure([]);
+            }
+
+            $migration = MigrationGenerator::generate(
+                $before,
+                $after,
                 new DropOnlySilentOperator()
+            )->getMigrationList()[0];
+
+            $expectedUp = file_get_contents($upPath);
+            $this->assertSame($expectedUp, $migration->getUp());
+            $expectedDown = file_get_contents($downPath);
+            $this->assertSame($expectedDown, $migration->getDown());
+
+            $pdo->exec('USE ' . DatabaseStructureFactory::TMP_DBNAME);
+
+            $pdo->exec(stripslashes($migration->getUp()));
+            $this->assertEquals(
+                $after,
+                DatabaseStructureFactory::fromPDO($pdo, DatabaseStructureFactory::TMP_DBNAME)
             );
-        } else {
-            $after = new DatabaseStructure([]);
-        }
 
-        if (file_exists($beforeDir)) {
-            $before = DatabaseStructureFactory::fromSqlDir(
-                $this->pdo,
-                $beforeDir,
-                new DropOnlySilentOperator(),
-                null,
-                false
+            $pdo->exec(stripslashes($migration->getDown()));
+            $this->assertEquals(
+                $before,
+                DatabaseStructureFactory::fromPDO($pdo, DatabaseStructureFactory::TMP_DBNAME)
             );
-        } else {
-            $this->pdo->exec('DROP DATABASE IF EXISTS ' . DatabaseStructureFactory::TMP_DBNAME);
-            $this->pdo->exec('CREATE DATABASE ' . DatabaseStructureFactory::TMP_DBNAME);
-            $before = new DatabaseStructure([]);
+
+            $pdo->exec(stripslashes($migration->getUp()));
+            $this->assertEquals(
+                $after,
+                DatabaseStructureFactory::fromPDO($pdo, DatabaseStructureFactory::TMP_DBNAME)
+            );
         }
-
-        $migration = MigrationGenerator::generate(
-            $before,
-            $after,
-            new DropOnlySilentOperator()
-        )->getMigrationList()[0];
-
-        $expectedUp = file_get_contents($upPath);
-        $this->assertSame($expectedUp, $migration->getUp());
-        $expectedDown = file_get_contents($downPath);
-        $this->assertSame($expectedDown, $migration->getDown());
-
-        $this->pdo->exec('USE ' . DatabaseStructureFactory::TMP_DBNAME);
-
-        $this->pdo->exec(stripslashes($migration->getUp()));
-        $this->assertEquals(
-            $after,
-            DatabaseStructureFactory::fromPDO($this->pdo, DatabaseStructureFactory::TMP_DBNAME)
-        );
-
-        $this->pdo->exec(stripslashes($migration->getDown()));
-        $this->assertEquals(
-            $before,
-            DatabaseStructureFactory::fromPDO($this->pdo, DatabaseStructureFactory::TMP_DBNAME)
-        );
-
-        $this->pdo->exec(stripslashes($migration->getUp()));
-        $this->assertEquals(
-            $after,
-            DatabaseStructureFactory::fromPDO($this->pdo, DatabaseStructureFactory::TMP_DBNAME)
-        );
     }
 
     public function generateProvider()
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@ set_time_limit(0);
 
 require dirname(__FILE__) . '/../vendor/autoload.php';
 
-$pdo = \Laminaria\Conv\TestUtility::getPdo();
-
-$pdo->exec('DROP DATABASE IF EXISTS conv_test');
-$pdo->exec('CREATE DATABASE conv_test');
+foreach (\Laminaria\Conv\TestUtility::getPdoArray() as $pdo) {
+    $pdo->exec('DROP DATABASE IF EXISTS conv_test');
+    $pdo->exec('CREATE DATABASE conv_test');
+}
